3bd3aac0ab3f0e79bea095c22ec2d472b928174e
Shubhan Agrawal <159256998+shubhanagrawal@users.noreply.github.com>
2025-11-09 19:25:41 +0530

    Enhance FCFS scheduling with Gantt chart display
    Added Gantt chart functionality and improved process input prompts.

--- ass3.c
+++ ass3.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
-void sort(int p[], int at[], int bt[], int n)
+void sort(int p[20], int at[20], int bt[20], int n)
 {
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n - i - 1; j++)
         {
             if (at[j] > at[j + 1])
             {
-                int temp = p[j];
-                p[j] = p[j + 1];
-                p[j + 1] = temp;
-
-                temp = at[j];
+                int temp = at[j];
                 at[j] = at[j + 1];
                 at[j + 1] = temp;
 
                 temp = bt[j];
                 bt[j] = bt[j + 1];
                 bt[j + 1] = temp;
+
+                temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
             }
         }
     }
 }
 
-void fcfs(int at[], int bt[], int ct[], int p[], int n, int tat[], int wt[])
+void fcfs(int at[20], int bt[20], int ct[20], int p[20], int n, int tat[20], int wt[20])
 {
-    ct[0] = at[0] + bt[0]; // first process completes after its burst time
-
+    ct[0] = at[0] + bt[0];
     for (int i = 1; i < n; i++)
     {
         if (ct[i - 1] < at[i])
@@ -47,27 +48,71 @@ void fcfs(int at[], int bt[], int ct[], int p[], int n, int tat[], int wt[])
     }
 }
 
-void findavg(int tat[], int wt[], int n)
+void gantt(int at[20], int bt[20], int ct[20], int n, int p[20])
 {
-    float stat = 0, swt = 0;
+    printf("\nGantt Chart:\n ");
 
     for (int i = 0; i < n; i++)
+        printf("--------");
+    printf("\n|");
+
+    int time = 0;
+    for (int i = 0; i < n; i++)
     {
-        stat += tat[i];
-        swt += wt[i];
+        if (time < at[i])
+        {
+            printf(" IDLE |");
+            time = at[i];
+        }
+        printf(" P%d |", p[i]);
+        time += bt[i];
     }
 
-    printf("\nAverage Turnaround Time = %.2f", stat / n);
-    printf("\nAverage Waiting Time = %.2f\n", swt / n);
+    printf("\n ");
+    for (int i = 0; i < n; i++)
+        printf("--------");
+
+    time = 0;
+    printf("\n0");
+    for (int i = 0; i < n; i++)
+    {
+        if (time < at[i])
+        {
+            printf("     %d", at[i]);
+            time = at[i];
+        }
+        time += bt[i];
+        printf("      %d", time);
+    }
+    printf("\n");
 }
 
-void print(int p[], int at[], int bt[], int ct[], int tat[], int wt[], int n)
+void findavg(int tat[20], int wt[20], int n)
 {
-    printf("\nP\tAT\tBT\tCT\tTAT\tWT\n");
+    int tatsum = 0;
+    int wtsum = 0;
+
+    for (int i = 0; i < n; i++)
+        tatsum += tat[i];
+
+    for (int i = 0; i < n; i++)
+        wtsum += wt[i];
+
+    float atat = (float)tatsum / n;
+    float awt = (float)wtsum / n;
+
+    printf("\nAverage Turnaround Time = %.2f", atat);
+    printf("\nAverage Waiting Time = %.2f\n", awt);
+}
+
+void print(int at[20], int bt[20], int ct[20], int tat[20], int wt[20], int n)
+{
+    printf("\nAT\tBT\tCT\tTAT\tWT");
     for (int i = 0; i < n; i++)
     {
-        printf("%d\t%d\t%d\t%d\t%d\t%d\n", p[i], at[i], bt[i], ct[i], tat[i], wt[i]);
+        printf("\n%d\t%d\t%d\t%d\t%d", at[i], bt[i], ct[i], tat[i], wt[i]);
     }
+    printf("\n");
 }
 
 int main()
@@ -78,14 +123,15 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        printf("Enter Process No, Arrival Time and Burst Time: ");
+        printf("\nEnter Process No, Arrival Time and Burst Time for process %d: ", i + 1);
         scanf("%d %d %d", &p[i], &at[i], &bt[i]);
     }
 
     sort(p, at, bt, n);
     fcfs(at, bt, ct, p, n, tat, wt);
-    print(p, at, bt, ct, tat, wt, n);
+    print(at, bt, ct, tat, wt, n);
     findavg(tat, wt, n);
+    gantt(at, bt, ct, n, p);
 
     return 0;
 }
